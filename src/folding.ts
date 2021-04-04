421423fca38f4d08a090530964c217b94156d849
ambrt <ambrth@gmail.com>
2021-04-04 16:12:03 +0200

    compatibility with RichMarkdown image, fixes #1

--- src/folding.ts
+++ src/folding.ts
@@ -8,123 +8,170 @@ import { disconnect } from "node:process";
 function plugin(CodeMirror) {
 
 	let indicator = "."
-	CodeMirror.defineExtension('persistentFolding', function() {
-		
+	CodeMirror.defineExtension('persistentFolding', function () {
+
 		this.on('change', on_change);
 		this.on('update', on_change);
-		
+
 		this.on("gutterClick", on_gutter);
-		
-	
-		
+
+
+
 	});
-	
-	function on_gutter(cm, lineNr, gutter){
+
+	function on_gutter(cm, lineNr, gutter) {
 		console.log('gutter line clicked')
 		let doc = cm.getDoc()
-
-		
-
 		let line = doc.getLineHandle(lineNr)
-		if("gutterMarkers" in line){
-			if("CodeMirror-foldgutter" in line.gutterMarkers){
-		
+		function myLine(lineEach) {
 
-		let lineOldText = line.text
-		
-		let lastChar = lineOldText.charAt(lineOldText.length-1)
-		//TODO: check if gutter click is only on headdings or normal lines too
-		if(lastChar==indicator){
-			let lastLength = lineOldText.length
-			doc.replaceRange(lineOldText.slice(0,-1),{line:lineNr, ch:0 },{line:lineNr, ch:lastLength})
-		}else{
-			let lastLength = lineOldText.length
-			doc.replaceRange(lineOldText+indicator,{line:lineNr, ch:0 },{line:lineNr, ch:lastLength+1})
+			if (lineEach.text.includes("![")) {
+				
+					let richMdNr = doc.getLineNumber(lineEach)
+					let nextRichMdNr = richMdNr + 1
+					console.log(line)
+					try{
+						let nextRichMdLine = doc.getLineHandle(nextRichMdNr)
+						console.log(nextRichMdLine)
+						console.log(nextRichMdLine.text.indexOf("#") )
+					if (nextRichMdLine.text.indexOf("#") == 0) {
+						var pos = { // create a new object to avoid mutation of the original selection
+							line: lineEach.text,
+							ch: lineEach.text.length - 1 // set the character position to the end of the line
+						}
+						doc.replaceRange("\n"+nextRichMdLine.text, { line: nextRichMdNr, ch: 0 }, { line: nextRichMdNr, ch: nextRichMdLine.text.length + 1 })
+						cm.foldCode(CodeMirror.Pos(lineNr, lineEach.text.length))
+						cm.foldCode(CodeMirror.Pos(lineNr, lineEach.text.length))
+
+					}
+				}catch(err){
+					console.log(err)
+					let lastEditorLineNr = doc.lastLine()
+					let lastEditorLine = doc.getLineHandle(lastEditorLineNr)
+					console.log(lastEditorLine)
+					doc.replaceRange(lastEditorLine.text+"\n", { line: lastEditorLineNr, ch: 0 }, { line: lastEditorLineNr, ch: lastEditorLine.text.length + 1 })
+					cm.foldCode(CodeMirror.Pos(lineNr, lineEach.text.length))
+					cm.foldCode(CodeMirror.Pos(lineNr, lineEach.text.length))
+				}
+
+				}
+			
 		}
-	}}
+		doc.eachLine(myLine)
+
+
+
+
+		
+		if ("gutterMarkers" in line) {
+			if ("CodeMirror-foldgutter" in line.gutterMarkers) {
+
+
+				let lineOldText = line.text
+				//let lastLine = line.parent.lines[line.parent.lines.length - 1]
+				let lastChar = lineOldText.charAt(lineOldText.length - 1)
+				if (lastChar == indicator) {
+					//unfold
+					let lastLength = lineOldText.length
+					doc.replaceRange(lineOldText.slice(0, -1), { line: lineNr, ch: 0 }, { line: lineNr, ch: lastLength })
+				} else {
+					//fold
+
+			
+					let lastLength = lineOldText.length
+					doc.replaceRange(lineOldText + indicator, { line: lineNr, ch: 0 }, { line: lineNr, ch: lastLength + 1 })
+					
+				}
+			}
+		}
 		//let marks = doc.getAllMarks()
 	}
 	function on_change(cm, change) {
 		console.log("change")
 		// Changed note here
 		// Fold all to be folded
-	let doc = cm.getDoc()
-	//console.log(doc)
-	//console.log(change)
-	let res = {
-        value: cm.getValue(),
-        selections: doc.listSelections(),
-        marks: [], 
-		allCollapsible:[]
-      };
+		let doc = cm.getDoc()
+		//console.log(doc)
+		//console.log(change)
+		let res = {
+			value: cm.getValue(),
+			selections: doc.listSelections(),
+			marks: [],
+			allCollapsible: []
+		};
+
+		let marks = doc.getAllMarks()
+		//console.log(marks)
+		/*
+		if ( marks.length ){
+			// We reverse the array in order to start in the last folded parts in case of nesting
+			for ( var i = marks.length - 1; i >= 0; i-- ){
+				//res.marks.push(marks[i].find().from);
+				
+			  if ( marks[i].collapsed && (marks[i].type === 'range') ){
+				res.marks.push(marks[i].find().from);
+				console.log(doc.getLine(marks[i].find().from.line))
 	
-	let marks = doc.getAllMarks()
-	/*
-	if ( marks.length ){
-		// We reverse the array in order to start in the last folded parts in case of nesting
-		for ( var i = marks.length - 1; i >= 0; i-- ){
-			//res.marks.push(marks[i].find().from);
+	
+			  }
+			  
+			}
 			
-		  if ( marks[i].collapsed && (marks[i].type === 'range') ){
-			res.marks.push(marks[i].find().from);
-			console.log(doc.getLine(marks[i].find().from.line))
-
-
 		  }
-		  
-		}
+		  */
+		let lastEditorLine = cm.lastLine()
 		
-	  }
-	  */
-	 console.log(marks.length)
-	 if(marks.length==0){
-		let guttered = []
-		function myLine(line){
-		  if("gutterMarkers" in line){
-			  if("CodeMirror-foldgutter" in line.gutterMarkers){
-				  //console.log(line)
-				  let lineNumber = doc.getLineNumber(line)
-				  //cm.foldCode(CodeMirror.Pos(lineNumber))
-				  guttered.push({line:line, ln:lineNumber})
-			  }
-		  }
-		  
-			
-		}
-		doc.eachLine(myLine)
-		console.log(guttered)
-		if ( guttered.length ){
-		  // We reverse the array in order to start in the last folded parts in case of nesting
-		  for ( var i = guttered.length - 1; i >= 0; i-- ){
-  
-			  let line = guttered[i].line
-			  let lineOldText = line.text
-			  
-			  let lastChar = lineOldText.charAt(lineOldText.length-1)
-			  
-			  if(lastChar==indicator){
-				  
-				  cm.foldCode(CodeMirror.Pos(guttered[i].ln,guttered[i].line.text.length))		
-				  
-			  }
-	  
-  
-			
-			
-		  }
-		  
+		console.log(marks.length)
+		if (marks.length == 0) {
+			let guttered = []
+			function myLine(line) {
+				
+				if ("gutterMarkers" in line) {
+					if ("CodeMirror-foldgutter" in line.gutterMarkers) {
+						//console.log(line)
+						let lineNumber = doc.getLineNumber(line)
+						//cm.foldCode(CodeMirror.Pos(lineNumber))
+						guttered.push({ line: line, ln: lineNumber })
+					}
+				}
+				
+		
+			}
+			doc.eachLine(myLine)
+
+			console.log(guttered)
+			if (guttered.length) {
+				// We reverse the array in order to start in the last folded parts in case of nesting
+				for (var i = guttered.length - 1; i >= 0; i--) {
+
+					let line = guttered[i].line
+					let lineOldText = line.text
+
+					let lastChar = lineOldText.charAt(lineOldText.length - 1)
+
+					if (lastChar == indicator) {
+
+						cm.foldCode(CodeMirror.Pos(guttered[i].ln, guttered[i].line.text.length))
+
+					}
+
+
+
+
+				}
+
+			}
+
 		}
 
-	 }
-	  
 
 	}
-	
-	CodeMirror.defineOption('enable-folding-mode', false, async function(cm, val, old) {
+
+	CodeMirror.defineOption('enable-folding-mode', false, async function (cm, val, old) {
 		// Cleanup
 		if (old && old != CodeMirror.Init) {
 			cm.off('change', on_change);
-		
+
 
 		}
 		// setup
@@ -134,9 +181,9 @@ function plugin(CodeMirror) {
 			// This code will perform an exponential backoff and poll settings
 			// until something is returned
 			async function backoff(timeout: number) {
-				
-					cm.persistentFolding()
-				
+
+				cm.persistentFolding()
+
 			};
 			// Set the first timeout to 50 because settings are usually ready immediately
 			// Set the first backoff to (100*2) to give a little extra time
@@ -147,31 +194,31 @@ function plugin(CodeMirror) {
 
 }
 module.exports = {
-	default: function(_context) { 
+	default: function (_context) {
 		return {
-            plugin:plugin,
+			plugin: plugin,
 			// Some resources are included with codemirror and extend the functionality in standard ways
 			// via plugins (called addons) which you can find here: https://codemirror.net/doc/manual.html#addons
 			// and are available under the addon/ directory
 			// or by adding keymaps under the keymap/ directory
 			// or additional modes available under the mode/ directory
 			// All are available in the  CodeMirror source: https://github.com/codemirror/codemirror
-			codeMirrorResources: ['addon/fold/foldgutter','addon/fold/markdown-fold'],
+			codeMirrorResources: ['addon/fold/foldgutter', 'addon/fold/markdown-fold'],
 			// Often addons for codemirror need to be enabled using an option,
 			// There is also certain codemirror functionality that can be enabled/disabled using
 			// simple options
 			codeMirrorOptions: {
-                foldGutter: true,
+				foldGutter: true,
 				'enable-folding-mode': true,
-                gutters: ["CodeMirror-foldgutter"]
+				gutters: ["CodeMirror-foldgutter"]
 
-            },
+			},
 			// More complex plugins (and some addons) will require additional css styling
 			// which is available through the assets function. As seen below, this styling can
 			// either point to a css file in the plugin src directory or be included inline.
-			assets: function() {
+			assets: function () {
 				return [
-					{name:"foldgutter.css"}
+					{ name: "foldgutter.css" }
 				];
 			},
 		}
